0b42a61357361d9f03133bc7033d4190f112ed55
AminAlih47 <AminAlih47@gmail.com>
2023-11-28 02:06:24 +0330

    Add scraper

---
+++ scraper/base.py
@@ -0,0 +1,16 @@
+from abc import ABC, abstractmethod
+
+from playwright.async_api import Playwright, Page
+
+
+class BaseScraper(ABC):
+    def __init__(self, playwright: Playwright):
+        self.playwright = playwright
+
+    async def _config_playwright(self) -> Page:
+        playwright = self.playwright
+        browser = await playwright.firefox.launch(headless=False)
+        return await browser.new_page()
+
+    async def _authenticate(self, username: str, password: str):
+        ...
---
+++ scraper/linkedin.py
@@ -0,0 +1,52 @@
+import asyncio
+from scraper.base import BaseScraper
+from config import envs
+from playwright.async_api import Error
+from scraper import xpaths
+
+
+class LinkedScraper(BaseScraper):
+    BASE_URL = envs.LINKEDIN_BASE_URL
+
+    async def _authenticate(self, username: str, password: str) -> None:
+        page = self.page
+
+        await page.goto(f'{self.BASE_URL}/uas/login/')
+
+        await page.locator('#username').fill(username)
+        await page.locator('#password').fill(password)
+        await page.locator(
+            '#organic-div > form > div.login__form_action_container > button'
+        ).click()
+
+    async def _search_people(self, envelope: str) -> str | dict:
+        page = self.page
+
+        search_url = f'{self.BASE_URL}search/results/people/?keywords={envelope}'
+
+        await page.goto(search_url, wait_until='load')
+
+        try:
+            people_list = page.locator(xpaths.PEOPLE_LIST_XPATH)
+        except Error:
+            page.reload(wait_until='load')
+
+            try:
+                people_list = page.locator(xpaths.PEOPLE_LIST_XPATH)
+            except Error:
+                return 'No results'
+
+        x = await people_list.inner_html()
+        print(x)
+
+    async def run(self, envelope: str):
+        self.page = await self._config_playwright()
+
+        await self._authenticate(
+            username=envs.LINKEDIN_USERNAME,
+            password=envs.LINKEDIN_PASSWORD,
+        )
+        await asyncio.sleep(2)
+        await self._search_people(envelope=envelope)
+
+        await asyncio.sleep(10)
